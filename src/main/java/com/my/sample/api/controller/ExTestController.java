8eea8261a1154e89399e0f6a81509dc120749a48
shiyongbiao <shiyongbiao@incar100.com>
2018-11-05 13:29:47 +0800

    添加单元测试

--- src/main/java/com/my/sample/api/controller/ExTestController.java
+++ src/main/java/com/my/sample/api/controller/ExTestController.java
@@ -1,14 +1,18 @@
 package com.my.sample.api.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.HttpClientErrorException;
+
+import java.nio.charset.Charset;
 
 @RestController
 public class ExTestController {
 
     @GetMapping("ex")
     public String ex() {
-        throw new UnsupportedOperationException("测试异常返回");
+        throw new RuntimeException("测试返回异常");
     }
 
     @GetMapping("bind")
--- src/test/java/com/my/sample/api/SampleApiApplicationTests.java
+++ src/test/java/com/my/sample/api/SampleApiApplicationTests.java
@@ -1,18 +1,29 @@
 package com.my.sample.api;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.my.sample.api.model.form.UserAddForm;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.NestedServletException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @Slf4j
@@ -47,6 +58,17 @@ public class SampleApiApplicationTests {
     }
 
     @Test
+    public void exText() throws Exception {
+        try {
+            mvc.perform(get("/ex"))
+                    .andExpect(status().isInternalServerError())
+                    .andDo(print());
+        } catch (NestedServletException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void pingTest() throws Exception {
         String result = mvc.perform(get("/ping"))
                 .andExpect(handler().methodName("ping"))
@@ -54,6 +76,19 @@ public class SampleApiApplicationTests {
                 // jsonPath用法参考 https://github.com/json-path/JsonPath#operators
                 .andExpect(jsonPath("$.attachment", is("PONG")))
                 .andExpect(jsonPath("$.msg_code", is(200)))
+                .andReturn().getResponse().getContentAsString();
+        log.info("ping result: {}", result);
+    }
+
+    @Test
+    public void userAddTest() throws Exception {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("name", "测试名称");
+        map.add("age", "12");
+        map.add("address", "北京");
+
+        String result = mvc.perform(post("/users").params(map))
+                .andExpect(status().is4xxClientError())
                 .andReturn().getResponse().getContentAsString();
         log.info("ping result: {}", result);
     }
